e45be80171805214bd2db09e31e36973a8ecbb55
Tom Bailey <tombailey1@hotmail.co.uk>
2017-02-08 21:31:51 +0000

    Add stubbed ProxyStatusActivity

---
+++ app/src/main/java/me/tombailey/store/ProxyStatusActivity.java
@@ -0,0 +1,13 @@
+package me.tombailey.store;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tomba on 08/02/2017.
+ */
+
+public class ProxyStatusActivity extends AppCompatActivity {
+
+    //TODO: impl
+
+}
