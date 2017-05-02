0eed44cddbfe6b3473d185ad92d9d037d438f772
Tom Bailey <tombailey1@hotmail.co.uk>
2017-05-02 01:01:13 +0100

    Remove boilerplate javadoc

--- app/src/androidTest/java/me/tombailey/store/MainActivityTest.java
+++ app/src/androidTest/java/me/tombailey/store/MainActivityTest.java
@@ -13,11 +13,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
